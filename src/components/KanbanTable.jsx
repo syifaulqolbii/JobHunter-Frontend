f0b64e9e8060e11cba698fb48baf7a658fe5d521
Syifaul Qolbi <syifaulqolbi32@gmail.com>
2023-11-22 22:31:49 +0700

    add skeleton loading in kanban table

--- src/components/KanbanTable.jsx
+++ src/components/KanbanTable.jsx
@@ -3,12 +3,14 @@ import {getAllKanbans} from "../modules/fetch/kanban";
 
 const KanbanTable = () => {
     const [kanbans, setKanbans] = useState([])
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         try {
             const fetchKanbans = async () => {
                 const response = await getAllKanbans()
                 const kanbansData = response.data;
                 setKanbans(kanbansData)
+                setLoading(false)
             }
             fetchKanbans()
         } catch (e) {
@@ -37,27 +39,33 @@ const KanbanTable = () => {
                 </tr>
                 </thead>
                 <tbody>
-                {kanbans.map((kanban, index) => (
-                    <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <th scope="row"
-                            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {kanban.User.name}
-                        </th>
-                        <td className="px-6 py-4">
-                            {kanban.Job.job_name}
-                        </td>
-                        <td className="px-6 py-4">
-                            <div className="flex items-center">
-                                <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div>
-                                Accepted
-                            </div>
-                        </td>
-                        <td className="px-6 py-4">
-                            <a href="#"
-                               className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Detail</a>
-                        </td>
+                {loading ? (
+                    <tr className="animate-pulse">
+                        <td className="px-6 py-4 bg-gray-200 dark:bg-gray-700" colSpan="4"></td>
                     </tr>
-                ))}
+                ) : (
+                    kanbans.map((kanban, index) => (
+                        <tr key={index}
+                            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th scope="row"
+                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {kanban.User.name}
+                            </th>
+                            <td className="px-6 py-4">
+                                {kanban.Job.job_name}
+                            </td>
+                            <td className="px-6 py-4">
+                                <div className="flex items-center">
+                                    <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div>
+                                    Accepted
+                                </div>
+                            </td>
+                            <td className="px-6 py-4">
+                                <a href="#"
+                                   className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Detail</a>
+                            </td>
+                        </tr>
+                    )))}
                 </tbody>
             </table>
         </>
