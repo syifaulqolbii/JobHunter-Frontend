0753e4c9064b71fc81ff78ca2b5da419882857c0
Syifaul Qolbi <syifaulqolbi32@gmail.com>
2023-11-23 21:48:00 +0700

    push consume api + update status

--- src/modules/axios/index.js
+++ src/modules/axios/index.js
@@ -8,7 +8,7 @@ const instance = axios.create({ baseURL });
 instance.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+        config.headers.Authorization = token;
     }
     return config;
 });
