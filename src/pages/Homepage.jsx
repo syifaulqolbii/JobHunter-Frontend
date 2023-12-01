3168d0b04512cb977997217fa3a23f9e7b18b1bc
Syifaul Qolbi <syifaulqolbi32@gmail.com>
2023-12-01 20:52:24 +0700

    delete sidebar Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,8 @@
-import Sidebar from "../components/Sidebar.jsx";
-
 import React from 'react';
 
 const Homepage = () => {
   return (
     <div>
-      <Sidebar/>
       <h1>Welcome to the Homepage</h1>
       <p>This is a simple homepage for authenticated users.</p>
     </div>
