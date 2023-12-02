1e10e44f58f3c1dc407172016b6635f4e6be8c83
Syifaul Qolbi <syifaulqolbi32@gmail.com>
2023-12-02 10:03:21 +0700

    fix minor bug

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {useNavigate,Link} from 'react-router-dom';
+import {useNavigate, Link} from 'react-router-dom';
 import authAPI from '../modules/fetch/auth';
 import Navbar from "../components/Navbar.jsx";
 
@@ -33,71 +33,72 @@ const Register = () => {
 
     return (
         <>
-            <section class="bg-gray-50 dark:bg-gray-900">
-                <div class="flex flex-col my-14 items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <section className="bg-gray-50 dark:bg-gray-900">
+                <div className="flex flex-col my-14 items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Register
                         </h1>
                     </a>
                     <div
-                        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 
-                            <form class="space-y-4 md:space-y-6">
+                            <form className="space-y-4 md:space-y-6">
                                 <div>
-                                    <label for="first_name"
-                                           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                                    <label htmlFor="first_name"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
                                     <input type="text" id="first_name" value={name}
                                            onChange={(e) => setName(e.target.value)}
-                                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                            placeholder="Name" required/>
                                 </div>
                                 <div>
-                                    <label for="email"
-                                           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                                    <label htmlFor="email"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                                     <input type="email" name="email" id="email" value={email}
                                            onChange={(e) => setEmail(e.target.value)}
-                                           class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                            placeholder="name@mail.com" required=""/>
                                 </div>
                                 <div>
-                                    <label for="password"
-                                           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <label htmlFor="password"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                                     <input type="password" name="password" id="password" value={password}
                                            onChange={(e) => setPassword(e.target.value)} placeholder="••••••••"
-                                           class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                            required=""/>
                                 </div>
                                 <div>
-                                    <label for="confirm-password"
-                                           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
+                                    <label htmlFor="confirm-password"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
                                         password</label>
                                     <input type="confirm-password" name="confirm-password" id="confirm-password"
                                            value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}
                                            placeholder="••••••••"
-                                           class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                            required=""/>
                                 </div>
                                 <div>
-                                    <label for="role"
-                                           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
+                                    <label htmlFor="role"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
                                         Role</label>
                                     <select id="role" value={role} onChange={(e) => setRole(e.target.value)}
-                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                        <option selected>Choose a role</option>
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                         <option value="user">User</option>
                                         <option value="company">Company</option>
                                     </select>
                                 </div>
 
                                 <button type="button" onClick={handleRegister}
-                                        class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create
+                                        className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create
                                     Account
                                 </button>
                                 {error && <p style={{color: 'red'}}>{error}</p>}
-                                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Already have an account? <Link to="/login" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Already have an account? <Link to="/login"
+                                                                   className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login
+                                    here</Link>
                                 </p>
                             </form>
                         </div>
